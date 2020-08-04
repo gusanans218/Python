c5fd55f870889ab29532c664a3778308aee59f50
gusanans <s20035@gsm.hs.kr>
2020-08-05 01:00:27 +0900

    8.5

--- python1.py
+++ python1.py
@@ -6,4 +6,24 @@ if 5>2:
     y="hello, world!"
     print(x)
     print(y)
-    
+    x=5
+    y="john"
+    print(x)
+    print(y)
+    x,y,z="orange", "banana", "cherry"
+    print(x)
+    print(y)
+    print(z)
+    x=y=z="orange"
+    print(x)
+    print(y)
+    print(z)
+    x="awesome"
+    print("python is " +x)
+    x="python is"
+    y="awesome"
+    z=x+y
+    print(z)
+    x=5
+    y=10
+    print(x+y)
