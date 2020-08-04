895e5e9f6aeb792e0a75b41e6aaf7bd64a6a28db
gusanans <s20035@gsm.hs.kr>
2020-08-05 00:21:17 +0900

    8.4

--- test.py
+++ test.py
@@ -2,4 +2,11 @@ a=int(input("반지름: "))
 print("반지름은", a)
 b= int(input("반지름: "))
 print("반지름은 :", b)
-print("답 :", a*b*3.14)
+print("넓이 :", a*b*3.14)
+
+ 
+ a= int(input("키 :"))
+print("키는",a)
+b= int(input("몸무게 :"))
+print("몸무게는 :", b)
+print("합: ", a+b)
