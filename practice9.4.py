e1c63ee1bf408561c6862d5e173cfdd9117c6e5a
변승현 <67039023+gusanans218@users.noreply.github.com>
2020-09-04 23:32:13 +0900

    9.4
    9.4

---
+++ practice9.4.py
@@ -0,0 +1,24 @@
+# lang = 'python'
+# print(lang[0], lang[2])
+
+# license_plate = "24가 2210"
+# print(license_plate[-4:])
+
+# string = "홀짝홀짝홀짝냠냠"
+# print(string[::2])
+
+# string = "PYTHON"
+# print(string[::-1])
+
+# phone_number = "010-1111-2222"
+# phone_number1 = phone_number.replace("-", " ")
+# print(phone_number1)
+
+# phone_number = "010-1111-2222"
+# phone_number1 = phone_number.replace ('-', '')
+# print(phone_number1)
+
+# url = "http://sharebook.kr"
+# url_split = url.split ('.')
+# print(url_split[-1])
+
