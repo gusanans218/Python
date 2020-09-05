1b3052059f36df08a3a6a59bb25d6dd311e56be9
변승현 <67039023+gusanans218@users.noreply.github.com>
2020-09-05 23:23:45 +0900

    9.5
    9.5

---
+++ practice9.5.py
@@ -0,0 +1,146 @@
+# a = "3"
+# b = "4"
+# print(a+b)
+
+# print("Hi" * 3)
+
+# print("-" * 80)
+
+# t1 = "python"
+# t2 = "java"
+# t3 = t1 + ' ' + t2 + ' '
+# print(t3 * 4)
+
+# name1 = "김민수"
+# age1 = 10
+# name2 = "이철희"
+# age2 = 13
+# print("이름: %s 나이 : %d" % (name1, age1))
+# print("이름: %s 나이 : %d" % (name2, age2))
+
+# 과일 = ["사과", "귤", "수박"]
+# for 변수 in 과일:
+#     print(변수)
+
+# 과일 = ["사과", "귤", "수박"]
+# for 변수 in 과일:
+#     print("#####")
+
+# 변수 = "A"
+# print(변수)
+# 변수 = "B"
+# print(변수)
+# 변수 = "C"
+# print(변수)
+
+# 변수 = 10
+# print(변수)
+# 변수 = 20
+# print(변수)
+# 변수 = 30
+# print(변수)
+
+# 변수 = 100
+# print(변수 + 10)
+# 변수 = 200
+# print(변수 + 10)
+# 변수 = 300
+# print(변수 + 10)
+
+# 메뉴 = "김밥"
+# print("오늘의 메뉴 : " + 메뉴)
+# 메뉴 = "라면"
+# print("오늘의 메뉴 : " + 메뉴)
+# 메뉴 = "튀김"
+# print("오늘의 메뉴 : " + 메뉴)
+
+# 리스트 = ["김밥", "라면", "튀김"]
+# for 메뉴 in 리스트:
+#     print("오늘의 메뉴", 메뉴)
+
+# 리스트 = ["sk하이닉스", "삼성전자", "lg전자"]
+# for 종목명 in 리스트:
+#     길이 = len (종목명)
+#     print(길이)
+
+# 리스트 = ["dog", "cat", "parrot"]
+# for 동물이름 in 리스트:
+#     길이 = len(동물이름)
+#     print(동물이름, 길이)
+
+# 이름 = "dog"
+# print(이름[0])
+# 이름 = "cat"
+# print(이름[0])
+# 이름 = "parrot"
+# print(이름[0])
+
+# 리스트 = [1,2,3,4,5,6,7,8]
+# for 변수 in 리스트:
+#     print("3 x " +str(변수))
+
+# 리스트=["가", "나", "다", "라"]
+# 나다라 = 리스트[1:]
+# for 변수 in 나다라:
+#     print(변수)
+
+# 리스트 = ["가", "나", "다", "라"]
+# for 변수 in 리스트[: :2]:
+#     print(변수)
+
+# 리스트 = ["가", "나", "다", "라"]
+# for 변수 in 리스트[: :-1]:
+#     print(변수)
+
+# 리스트 = [3, -20, -3, 44]
+# for 변수 in 리스트 :
+#     if 변수 <0:
+#         print(변수)
+
+# 리스트 = [3,100,23,44]
+# for 변수 in 리스트:
+#     if 변수 % 3 ==0:
+#         print(변수)
+
+# 리스트 = [13, 21, 12, 14, 30, 18]
+# for 변수 in 리스트:
+#     if(변수<20) and (변수 % 3 == 0):
+#         print(변수)
+
+# 리스트 = ["I", "study", " python", "language", "!"]
+# for 변수 in 리스트:
+#     if len(변수) >=3:
+#         print(변수)
+
+# 리스트 = ["A", "b", "c", "D"]
+# for 변수 in 리스트:
+#      if 변수.isupper():
+#         print(변수)
+
+# 리스트 = ["A", "b", "c", "D"]
+# for 변수 in 리스트:
+#   if 변수.isupper() != True:
+#     print(변수)
+
+# 리스트 = ["dog", "cat", "parrot"]
+# for 변수 in 리스트 :
+#     첫글자 = 변수[0]
+#     대문자 = 첫글자.upper()
+#     print(대문자 + 변수[1:])
+
+# 리스트 = ['hello.py', 'ex01.py', 'intro.hwp']
+# for 변수 in 리스트:
+#     split = 변수.split(".")
+#     print(split[0])
+
+# 리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
+# for 변수 in 리스트:
+#     split = 변수.split(".")
+#     if split[1] == "h":
+#         print(변수)
+        
+# 리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
+# for 변수 in 리스트:
+#     split = 변수.split(".")
+#     if(split[1] == "h") or (split[1]=="c"):
+#         print(변수)
