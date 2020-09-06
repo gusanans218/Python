cfba534054f7aa3d36b1d90949c546c29479fcc0
변승현 <67039023+gusanans218@users.noreply.github.com>
2020-09-06 23:28:31 +0900

    Add files via upload

---
+++ practice9.6.py
@@ -0,0 +1,110 @@
+# movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
+# movie_rank.append("배트맨")
+# print(movie_rank)
+
+# movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
+# movie_rank.insert(1, "슈퍼맨")
+# print(movie_rank)
+
+# movie_rank = ["닥터 스트레인지", "슈퍼맨", "스플릿", "럭키", "배트맨"]
+# del movie_rank[3]
+# print(movie_rank)
+
+# movie_rank = ["닥터 스트레인지","슈퍼맨", "스플릿", "럭키"]
+# del movie_rank[2]
+# del movie_rank[2]
+# print(movie_rank)
+
+# lang1 = ["c", "c++", "java"]
+# lang2 = ["python", "go", "c#"]
+# langs = lang1+ lang2
+# print(langs)
+
+# nums  = [1,2,3,4,5,6,7]
+# print("max: ", max(nums))
+# print("min: ", min(nums))
+
+# nums = [1,2,3,4,5]
+# print(sum(nums))
+
+# cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+# print(len(cook))
+
+# nums = [1,2,3,4,5]
+# average = sum(nums)/len(nums)
+# print(average)
+
+# price = ['20180728', 100, 130, 140, 150, 160, 170]
+# print(price[1:])
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(nums[1::2])
+
+# nums = [1, 2, 3, 4, 5]
+# print(nums[::-1])
+
+# interest = ['삼성전자', 'lg전자', 'naver']
+# print(interest[0], interest[2])
+
+# interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+# print(" ".join(interest))
+
+# data = [2,4,3,1,5,10,9]
+# data.sort()
+# print(data)
+
+# data = tuple(range(2,100,2))
+# print(data)
+
+# scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
+# *valid_score, _, _ = scores
+# print(valid_score)
+
+# ice = {"메로나": 1000, "폴라포": 1200, "빵빠레": 1800}
+# ice["죠스바"] = 1200
+# ice["월드콘"] = 1500
+# print(ice)
+
+# if True : 
+#     if False:
+#         print("1")
+#         print("2")
+#     else:
+#         print("3")
+# else:
+#      print("4")
+# print("5")
+
+# user = input("")
+# if int (user) % 2 == 0:
+#     print("짝수")
+# else:
+#     print("홀수")
+
+# user = input("입력값: ")
+# num = 20 + int(user)
+# if num < 255:
+#     print(255)
+# else: 
+#     print(num)
+
+# time = input("현재시간")
+# if time[-2:] == "00":
+#     print("정각입니다")
+# else:
+#     print("정각이 아닙니다")
+
+# fruit = ["사과", "포도", "홍시"]
+# user = input("좋아하는 과일은? ")
+# if user in fruit:
+#     print("정답입니다")
+# else:
+#     print("오답입니다")
+
+# warn_investment_list = ["Microsoft", "Google", "Naver", "Kakao", "SAMSUNG", "LG"]
+# 종목 = input("종목망 : ")
+# if 종목 in warn_investment_list:
+#     print("투자 경고 종목입니다")
+# else:
+#     print("투자 경고 종목이 아닙니다")
+
