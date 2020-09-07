31b95c50ff2a98484965f71ff319c1daf768905e
변승현 <67039023+gusanans218@users.noreply.github.com>
2020-09-07 23:27:03 +0900

    200907
    200907

---
+++ practice9.7.py
@@ -0,0 +1,58 @@
+# for x in range(2002, 2051, 4) :
+#     print(x)
+
+# for num in range(3,31,3) :
+#     print(num)
+
+# for i in range(100) :
+#     print(99 - i)
+
+# for num in range(10) :
+#     print(num/10, num)
+
+# for i in range(1, 10) :
+#     print(3, "x", i, " = ", 3*i)
+
+# num = 3
+# for i in range(1, 10, 2) :
+#     print(num, "x", i, " = ", num*i)
+
+# hab = 0
+# for i in range(1,11):
+#     hab += i
+#     print("합: ", hab)
+
+# hab = 0
+# for i in range(1,11,2):
+#     hab += i
+#     print("합: ", hab)
+
+# result = 1
+# for i in range(1,11):
+#     result *= i
+#     print(result)
+
+# price_list = [32100, 32150, 32000, 32500]
+# for i in range(1,4):
+#     print(90 + 10 * i, price_list[i])
+
+# my_list = ["가", "나", "다", "라", "마"]
+# print(my_list[0], my_list[1])
+# print(my_list[1], my_list[2])
+# print(my_list[2], my_list[3])
+
+# apart = [ [101, 102], [201, 202], [301, 302] ]
+# for row in apart:
+#     for col in row:
+#         print(col, "호")
+#         print("-" *5)
+
+# data = [
+#     [ 2000,  3050,  2050,  1980],
+#     [ 7500,  2050,  2050,  1980],
+#     [15450, 15050, 15550, 14900]
+# ]
+# for line in data :
+#     for column in line:
+#         print(column * 1.00014)
+
