16873e8f1e7f18d84af6e869f09ea46d9ecf8838
변승현 <67039023+gusanans218@users.noreply.github.com>
2020-12-16 15:53:03 +0900

    20201216
    vscode

--- python2.py
+++ python2.py
@@ -1,16 +1,45 @@
-x= min(5,10,25)
-y= max(5,10,25)
-print(x)
-print(y)
+# def least_difference(a,b,c):
+#     diff1 = abs(a-b)
+#     diff2 = abs(b-c)
+#     diff3 = abs(a-c)
+#     min(diff1, diff2, diff3)
 
-x=abs(-7.25)
-print(x)
+# print(
+#     least_difference(1,10,100),
+#     least_difference(1,10,10),
+#     least_difference(5,6,7),
+# )
 
-def my_function():
-    print("hello from a function")
-    my_function()
+# mystery = print()
+# print(mystery)
 
-    def my_function(fname, lname):
-        print(fname+"" + lname)
+# print(1,2,3,sep='<')
 
-        my_function("emil", "refsnes")
+# def greet(who ="Colin"):
+#     print("Hello,", who)
+
+# greet()
+# greet(who="Kaggle")
+# greet("world")
+
+# def mult_by_five(x):
+#     return 5 * x
+# def call(fn, arg):
+#     return fn(arg)
+# def squared_call(fn, arg):
+#     return fn(fn(arg))dj 
+# print(
+#     call(mult_by_five,1),
+#     squared_call(mult_by_five,1),
+#     sep='\n',
+# )
+
+def mod_5(x):
+    return x % 5
+print(
+    'which number is biggest?',
+    max(100,51,14),
+    'which numberp is the biggest modulo 5?',
+    max(100,51,14,key=mod_5),
+    sep='\n',
+)
