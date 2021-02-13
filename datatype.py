be4abab27d93501431211752ab03261cd62e96ed
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-02-13 16:48:58 +0900

    20210213

--- datatype.py
+++ datatype.py
@@ -1,5 +1,31 @@
-#region
-head = "Python"
-tail = " is fun!"
-print(head+tail)
-#endregion
+# #region
+# head = "Python"
+# tail = " is fun!"
+# print(head+tail)
+# #endregion
+
+# #region
+# print("*" * 10)
+# #endregion
+
+# #region
+# print("한 줄 쓰고\n그 다음 줄을 쓴다.")
+# #endregion
+
+# print("한 줄 쓰고", end="")
+# print("이어서 쓴다.")
+
+# name = "홍길동"
+# print("내 이름은"+name+ "입니다")
+
+# mark = "$"
+# n = 20
+# print(mark + "기호를"+ str(n)+ "번 출력합니다")
+# print(mark *n)
+
+# print('둘리가 "호이!" 하고 말했어요.')
+# print("둘리가 '이제 어디로 가지?' 하고 생각했어요.")
+
+print("2020.10.23".replace(".", "-"))
+
+print("word with space".replace(" ",""))
